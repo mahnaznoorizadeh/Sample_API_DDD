284e8ca3e63034e253c8965db68705f7629e5c69
mahnaznoorizadeh <mza@f5it.com>
2023-01-17 15:27:07 +0100

    Add project files.

---
+++ Bad_TaxCalculator/IndividualTaxCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Bad_TaxCalculator;
+public class IndividualTaxCalculator
+{
+    public int CalculateTaxPercentage(TaxPayer taxPayer)
+    {
+        if (!taxPayer.TaxCitizen)
+            throw new InvalidOperationException("You are not tax citizen");
+        
+        return 0;
+    }
+}
